5541e21cbbd515e648ec538fa71739cb08ac1fd8
Amber Mills <amberdmills@live.com>
2024-12-13 13:00:13 -0600

    add properties to the model like the schema

--- Models/Task.cs
+++ Models/Task.cs
@@ -2,5 +2,13 @@
 {
     public class Task
     {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public string Priority { get; set; } = "MEDIUM";
+        public string Status { get; set; } = "TODO";
+        public DateTime? DueDate { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     }
 }
