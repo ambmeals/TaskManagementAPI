9e0ef17578e62cd4a336bb756f107a5a2514332b
Amber Mills <amberdmills@live.com>
2025-01-08 14:23:49 -0600

    add

--- Program.cs
+++ Program.cs
@@ -13,7 +13,10 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000", "https://localhost:3000", "http://localhost:3001", "https://localhost:3001")
+        policy.WithOrigins(
+                "http://localhost:3000",
+                "https://localhost:3000",
+                "https://timemanagementapp.azurewebsites.net")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
