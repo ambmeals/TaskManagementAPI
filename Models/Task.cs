34fdfbaeeec1197b5902345a411167b7366d0a7b
Amber Mills <amberdmills@live.com>
2025-01-12 14:47:02 -0600

    update

--- Models/Task.cs
+++ Models/Task.cs
@@ -5,8 +5,8 @@
         public string Id { get; set; } = Guid.NewGuid().ToString();
         public string? Title { get; set; }
         public string? Description { get; set; }
-        public string Priority { get; set; } = "MEDIUM";
-        public string Status { get; set; } = "TO DO";
+        public string? Priority { get; set; } = "MEDIUM";
+        public string? Status { get; set; } = "TO DO";
         public DateTime? DueDate { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
